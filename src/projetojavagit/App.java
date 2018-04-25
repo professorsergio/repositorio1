4fa41b1d9a55eaf5e66bce5f68f25586d4d8a09d
professorsergio <femass.sergio@gmail.com>
2018-04-25 10:57:48 -0300

    primeiro commit;

---
+++ src/projetojavagit/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetojavagit;
+
+/**
+ *
+ * @author Aspire1410
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro teste com git");
+    }
+    
+}
